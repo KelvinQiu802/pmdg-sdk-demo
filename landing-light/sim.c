4c29feda9bbb131a907ae562fb450deedb893afc
kelvinqiu802@outlook.com <kelvinqiu802@outlook.com>
2024-10-24 18:22:34 +0800

    feat: control fenix nose light

--- landing-light/sim.c
+++ landing-light/sim.c
@@ -3,52 +3,40 @@
 #include <SimConnect.h>
 
 struct Data {
-	double taxi_light;
-};
-
-enum EVENT_ID {
-	EVENT_TAXI_LIGHT_TOGGLE,
+	double nose_light;
 };
 
 enum REQUEST_ID {
-	REQUEST_TAXI_LIGHT
+	REQUEST_NOSE_LIGHT
 };
 
 enum DEFINE_ID {
-	DEFINE_TAXI_LIGHT
+	DEFINE_NOSE_LIGHT
 };
 
-enum GROUP_ID {
-	HIGHEST_PRIORITY_GROUP
+struct LvarData {
+	double value;
 };
 
 HANDLE hSimConnect = NULL;
 
 void CALLBACK MyDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext) {
 	SIMCONNECT_RECV_SIMOBJECT_DATA* pObjData;
-	SIMCONNECT_RECV_EVENT* evt;
-	SIMCONNECT_RECV_OPEN* openData;
+	SIMCONNECT_RECV_EXCEPTION* except;
 	switch (pData->dwID) {
 	case SIMCONNECT_RECV_ID_SIMOBJECT_DATA:
 		pObjData = (SIMCONNECT_RECV_SIMOBJECT_DATA*)pData;
-		if (pObjData->dwRequestID == REQUEST_TAXI_LIGHT) {
+		if (pObjData->dwRequestID == REQUEST_NOSE_LIGHT) {
 			struct Data* pS = (struct Data*)&pObjData->dwData;
-			printf("Taxi Light: %s\n", pS->taxi_light == 1.0 ? "On" : "Off");
+			printf("Nose Light: %.0f\n", pS->nose_light);
 		}
 		else {
 			printf("Unknown request ID: %d\n", pObjData->dwRequestID);
 		}
 		break;
-	case SIMCONNECT_RECV_ID_EVENT:
-		evt = (SIMCONNECT_RECV_EVENT*)pData;
-		if (evt->uEventID == EVENT_TAXI_LIGHT_TOGGLE) {
-			// Toggle Taxi Light
-			break;
-		}
-		break;
-	case SIMCONNECT_RECV_ID_OPEN:
-		openData = (SIMCONNECT_RECV_OPEN*)pData;
-		printf("Connected to %s\n", openData->szApplicationName);
+	case SIMCONNECT_RECV_ID_EXCEPTION:
+		except = (SIMCONNECT_RECV_EXCEPTION*)pData;
+		printf("Exception: %d\n", except->dwException);
 		break;
 	default:
 		printf("Unknown packet ID: %d\n", pData->dwID);
@@ -62,27 +50,26 @@ int main() {
 	if (SUCCEEDED(SimConnect_Open(&hSimConnect, "SimConnect Test", NULL, 0, 0, 0))) {
 		printf("Connected to Flight Simulator!\n");
 
-		// Object Data
-		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINE_TAXI_LIGHT, "LIGHT TAXI", "Bool");
-		
-		// Event
-		hr = SimConnect_MapClientEventToSimEvent(hSimConnect, EVENT_TAXI_LIGHT_TOGGLE, "TOGGLE_TAXI_LIGHTS");
-		hr = SimConnect_AddClientEventToNotificationGroup(hSimConnect, HIGHEST_PRIORITY_GROUP, EVENT_TAXI_LIGHT_TOGGLE);
-		hr = SimConnect_SetNotificationGroupPriority(hSimConnect, HIGHEST_PRIORITY_GROUP, SIMCONNECT_GROUP_PRIORITY_HIGHEST);
-
+		// Object Data  
+		hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINE_NOSE_LIGHT, "L:S_OH_EXT_LT_NOSE", "Enum", SIMCONNECT_DATATYPE_FLOAT64);
+		hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_NOSE_LIGHT, DEFINE_NOSE_LIGHT, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME, SIMCONNECT_CLIENT_DATA_REQUEST_FLAG_CHANGED);
 
 		while (1) {
-			printf("Press 'q' to quit, '1' to toggle taxi light: ");
-			char c = getchar();
-			getchar();
-			if (c == '1') {
-				hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_TAXI_LIGHT, DEFINE_TAXI_LIGHT, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_ONCE);
-				hr = SimConnect_TransmitClientEvent(hSimConnect, SIMCONNECT_OBJECT_ID_USER, EVENT_TAXI_LIGHT_TOGGLE, 0, HIGHEST_PRIORITY_GROUP, SIMCONNECT_EVENT_FLAG_GROUPID_IS_PRIORITY);
-				SimConnect_CallDispatch(hSimConnect, MyDispatchProc, NULL);
+			printf("Set Nose Light -> ");
+			int nosepos;
+			if (scanf_s("%d", &nosepos) == 1) {
+				if (nosepos == -1) break;
+				LvarData data = { (double)nosepos };
+				hr = SimConnect_SetDataOnSimObject(hSimConnect, DEFINE_NOSE_LIGHT, SIMCONNECT_OBJECT_ID_USER, NULL, 0, sizeof(LvarData), &data);
+				printf("%s\n", hr == S_OK ? "Success" : "Failed");
 			}
-			if (c == 'q' || c == 'Q') {
-				break;
+			else {
+				printf("Invalid input! Please enter a valid integer.\n");
+				// Clear the input buffer  
+				while (getchar() != '\n');
+				continue;
 			}
+			SimConnect_CallDispatch(hSimConnect, MyDispatchProc, NULL);
 		}
 
 		hr = SimConnect_Close(hSimConnect);
